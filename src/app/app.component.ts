733d59dacd8ef0364de56cce056912c2fbb88a3f
Abdul Basit <bashialy@gmail.com>
2019-07-10 16:46:20 +0500

    Email Element Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { HttpClient} from '@angular/common/http';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -14,39 +18,50 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ]
 })
 export class AppComponent implements OnInit, ControlValueAccessor {
+
   @Input() label = '';
   @Input() placeholder = '';
+  @Input() model;
   // tslint:disable-next-line: variable-name
   private _value;
+  private _values = this.model;
+   isEmailUpdate =new Subject<string>();
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
-
-  public get value() {
-    console.log('get value called', this._value);
-    return this._value;
+  public get values() {
+    this.model = this._values;
+    return this.model;
   }
 
-  public set value(v) {
-    this._value = v;
-    this.onChange(this._value);
+  public set values(v) {
+    this._values = v;
+    this.onChange(this._values);
+    this.onChange(this.model);
     this.onTouched();
-    console.log('set value called', this.value, v);
+    this.model = v;
+    console.log('set value called', this.values, v);
   }
 
   writeValue(obj: any): void {
-    this._value = obj;
-    console.log('writeValue called', this.value, obj);
+    this._values = obj;
+    this.model = obj;
+    console.log('writeValue called', this.values, obj);
   }
 
   // Optional
   onSomeEventOccured(newValue) {
-    this._value = newValue;
+    this._values = newValue;
+    this.model = newValue;
   }
 
   ngOnInit() {
-    console.log('Initiated.');
+  this.isEmailUpdate.pipe(
+debounceTime(800),
+distinctUntilChanged())
+.subscribe(value => {
+this.checkForValidity(this.model.email)
+});
   }
-
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
@@ -56,15 +71,28 @@ export class AppComponent implements OnInit, ControlValueAccessor {
   }
 
   setDisabledState?(isDisabled: boolean): void {
-    console.log('setDisabledState called', this.value, isDisabled);
+    console.log('setDisabledState called', this.values, isDisabled);
   }
 
   onChange: any = () => {
-    console.log('onchange called', this.value)
+    console.log('onchange called', this.values)
   }
 
   onTouched: any = () => { 
-    console.log('onTouched called', this.value)
+    console.log('onTouched called', this.values)
+  }
+
+  checkForValidity(email) {
+this.httpClient.get('//apilayer.net/api/check?access_key=bbb071970d3ed2aaf30c168e2181bd51&email=' + email).subscribe((data: any) => {
+      let res = data;
+      if(res && res.format_valid) {
+        this.model.isValid = true;
+      } else if(res && !res.format_valid){
+        this.model.isValid = false;
+      } else if(!res.success) {
+        this.model.isValid = -1;
+      }
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [],
